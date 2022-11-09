3b40085963e9c54f96dafd8b53e60bf19dadf0fd
Dulguun <dulguun.web@gmail.com>
2022-11-09 22:03:47 +0800

    did nothing

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>asdsda</h1>
+      <div className=" bg-white border">asfasd</div>
+    </div>
+  );
+}
+
+export default Header;
